10105e44c631cfb4a4bee902c7bd74f56af65f17
comercio2007 <kargermistk@gmail.com>
2025-09-16 19:33:58 +0200

    Delete src/app/actions.ts

--- src/app/actions.ts
+++
@@ -1,4 +0,0 @@
-'use server';
-
-// This file is intentionally left blank as the mockup generation is now handled on the client side.
-// You can add other server actions here if needed in the future.
